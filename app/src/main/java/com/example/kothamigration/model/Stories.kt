733c52668e22d922cb50376b0d7119ff60632cf4
Rashadul Islam Himu <rashadulislamhimu@gmail.com>
2023-11-12 04:09:16 +0600

    Data Classes For User ,Post And Stories

---
+++ app/src/main/java/com/example/kothamigration/model/Stories.kt
@@ -0,0 +1,12 @@
+package com.example.kothamigration.model
+
+import com.example.kothamigration.R
+
+
+/**Stories Data */
+data class Stories(
+    val userName : String = "",
+    val profile : Int ,
+
+)
+

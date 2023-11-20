c0673af8930b7d37e3f2467d88927880be0cdae5
Rashadul Islam Himu <rashadulislamhimu@gmail.com>
2023-11-21 05:44:35 +0600

    Updated NavHost

--- app/src/main/java/com/example/kothamigration/model/ComposeBridge.kt
+++ app/src/main/java/com/example/kothamigration/model/ComposeBridge.kt
@@ -5,6 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
 import androidx.fragment.app.Fragment
@@ -16,8 +20,13 @@ import androidx.navigation.compose.rememberNavController
 import com.example.kothamigration.SwitchModeScreen
 import com.example.kothamigration.app.OnBoarding
 import com.example.kothamigration.app.ProjectApp
+import com.example.kothamigration.sellonboarding.PostCaptionScreen
+import com.example.kothamigration.sellonboarding.SelectUploadingVideo
 import com.example.kothamigration.sellonboarding.SellFrame1
 import com.example.kothamigration.sellonboarding.SellFrame2
+import com.example.kothamigration.sellonboarding.SellFrame3
+import com.example.kothamigration.sellonboarding.UploadedVideo
+import com.example.kothamigration.sellonboarding.UploadingPreviewScreen
 
 class ComposeBridge : Fragment() {
     fun callMyComposeScreen(composeView: ComposeView, navController: NavController) {
@@ -33,6 +42,9 @@ class ComposeBridge : Fragment() {
 @Composable
 fun OnBoardingContent() {
     val navController = rememberNavController()
+    var selectedItem by remember {
+        mutableStateOf<String?>(null)
+    }
 
     NavHost(
         navController = navController,
@@ -46,13 +58,49 @@ fun OnBoardingContent() {
         }
         composable("onboarding_screen_2") {
             SellFrame2 {
-                navController.navigate("mode")
+                navController.navigate("video_sell_frame_screen_3")
             }
         }
-        composable("mode") {
-            SwitchModeScreen(navController = navController)
+        composable("video_sell_frame_screen_3") {
+            SellFrame3{
+                navController.navigate("video_sell_frame_screen_4.1")
+
+            }
 
         }
+        composable("video_sell_frame_screen_4.1") {
+            SelectUploadingVideo{
+                navController.navigate("video_sell_frame_screen_4.2")
+
+            }
+
+        }
+        composable("video_sell_frame_screen_4.2") {
+            UploadedVideo{
+                navController.navigate("video_sell_frame_screen_5")
+
+            }
+
+        }
+        composable("video_sell_frame_screen_5") {
+            UploadingPreviewScreen{
+                navController.navigate("video_sell_frame_screen_6")
+
+            }
+
+        }
+
+        composable("video_sell_frame_screen_6") {
+            PostCaptionScreen{
+                navController.navigate("video_sell_frame_screen_7")
+
+            }
+
+        }
+
+
+
+
     }
 }
 
